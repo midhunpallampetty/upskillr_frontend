d829ae20a9ea216a70d49eb62e23b51ae52a2734
midhunpallampetty <midhunpallampetty@gmai.com>
2025-08-23 00:47:14 +0530

    added contact

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,30 +2,23 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PublicRoutes from './PublicRoutes';
 import SchoolRoutes from './SchoolRoutes';
-import MarketingPage from '../features/school/MarketingPage';
 
 interface Props {
   subdomain: string | null;
 }
 
-const AppRouter: React.FC<Props> = ({ subdomain }) => {
-  if (subdomain && subdomain !== '') {
-    return (
-      <Routes>
-        <Route path="/" element={<MarketingPage />} />
-        {/* Optional: Fallback for other paths on subdomain */}
-      </Routes>
-    );
-  }
 
-  // Main domain (eduvia.space) uses PublicRoutes
+const AppRouter: React.FC<Props> = ({ subdomain }) => {
   return (
     <Routes>
-      <Route path="*" element={<SchoolRoutes />} /> {/* Or a 404/redirect */}
-
-      <Route path="*" element={<PublicRoutes />} />
+      {subdomain ? (
+        <Route path="*" element={<SchoolRoutes />} />
+      ) : (
+        <Route path="*" element={<PublicRoutes />} />
+      )}
     </Routes>
   );
 };
+
 
 export default AppRouter;

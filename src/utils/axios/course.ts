20232924af6d49d86c1c91187302078b55ca29f3
midhunpallampetty <midhunpallampetty@gmai.com>
2025-08-23 19:32:08 +0530

    updated deletion

--- src/utils/axios/course.ts
+++ src/utils/axios/course.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const courseAxios = axios.create({
   baseURL: import.meta.env.VITE_COURSE_API_BASE,
-  withCredentials: false,
+  withCredentials: true,
 });
 
 export default courseAxios;

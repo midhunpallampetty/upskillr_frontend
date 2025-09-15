e9fa2a30ed72cfdbb195efd7a65264cc4ab7ce22
midhunpallampetty <midhunpallampetty@gmai.com>
2025-09-15 23:41:05 +0530

    added schoologin in subdomain

--- src/routes/SchoolRoutes.tsx
+++ src/routes/SchoolRoutes.tsx
@@ -1,25 +1,7 @@
 import React, { lazy } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import MarketingPage from '../features/school/MarketingPage';
 
-const StudentLogin = lazy(() => import('../features/student/studentLogin'));
 const VerifiedSchoolHome = lazy(() => import('../features/school/VerifiedSchoolHome'));
 
-const SchoolRoutes = () => {
-  const location = useLocation();
-  
-  // Show MarketingPage only for root path
-  if (location.pathname === '/') {
-    return <MarketingPage />;
-  }
-  
-  // Handle specific routes
-  if (location.pathname === '/studentLogin') {
-    return <StudentLogin />;
-  }
-  
-  // Default fallback
-  return <VerifiedSchoolHome />;
-};
+const SchoolRoutes = () => <VerifiedSchoolHome />;
 
 export default SchoolRoutes;

5785a2b788db37d1c323c805a14413301da42ce3
midhunpallampetty <midhunpallampetty@gmai.com>
2025-09-16 00:10:22 +0530

    added schoologin in subdomain

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PublicRoutes from './PublicRoutes';
 import MarketingPage from '../features/school/MarketingPage';
+import LandingPage from '../features/shared/Landing';
+import AdminAuth from '../features/admin/AdminAuth';
+import StudentLogin from '../features/student/studentLogin';
+import SchoolLogin from '../features/school/schoolLogin';
+import StudentRegister from '../features/student/studentRegister';
+import SchoolRegister from '../features/school/schoolRegister';
+import NotFound from '../features/shared/components/Layout/NotFound';
 
 interface Props {
   subdomain: string | null;
@@ -10,14 +17,21 @@ interface Props {
 const AppRouter: React.FC<Props> = ({ subdomain }) => {
   return (
     <Routes>
-      {/* Specific root route - must come BEFORE wildcard */}
+      {/* Handle root path based on subdomain */}
       <Route 
         path="/" 
-        element={subdomain ? <MarketingPage /> : <PublicRoutes subdomain={subdomain} />} 
+        element={subdomain ? <MarketingPage /> : <LandingPage />} 
       />
       
-      {/* All other paths go to PublicRoutes */}
-      <Route path="/*" element={<PublicRoutes subdomain={subdomain} />} />
+      {/* All other paths go to PublicRoutes - but flatten them */}
+      <Route path="/adminLogin" element={<AdminAuth />} />
+      <Route path="/studentLogin" element={<StudentLogin />} />
+      <Route path="/schoolLogin" element={<SchoolLogin />} />
+      <Route path="/studentRegister" element={<StudentRegister />} />
+      <Route path="/schoolRegister" element={<SchoolRegister />} />
+      {/* Add other important routes here */}
+      
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };

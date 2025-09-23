b43e3d8c39729b9e561c81ad18e74707c73cf818
midhunpallampetty <midhunpallampetty@gmai.com>
2025-09-23 11:05:50 +0530

    updated reset password

--- src/features/school/api/exam.controll.ts
+++ src/features/school/api/exam.controll.ts
@@ -1,33 +1,24 @@
-
-
 import courseAxios from '../../../utils/axios/course';
 import examAxios from '../../../utils/axios/exam';
-import { Exam } from '../types/Exam'; // Adjust based on your types
+import { apiRequest } from '../../../utils/apiRequest';
+import { Exam } from '../types/Exam';
 
+// Fetch all exams
 export const fetchAllExams = async (schoolName: string): Promise<Exam[]> => {
-  try {
-    const response = await examAxios.get('/exam/all-exgfams', { params: { schoolName } });
-    return response.data;
-  } catch (error: any) {
-    const message = error.response?.data?.message || 'Failed to fetch exams';
-    throw new Error(message);
-  }
+  return await apiRequest<Exam[]>(examAxios, 'get', '/exam/all-exams', null, {
+    params: { schoolName },
+  });
 };
 
+// Update course exam
 export const updateCourseExam = async (
   schoolName: string,
   courseId: string,
   examId: string,
   examType: 'preliminary' | 'final'
 ) => {
-  try {
-    const response = await courseAxios.put(`/${schoolName}/courses/${courseId}/exams`, {
-      examId,
-      examType,
-    });
-    return response.data; // Contains any response data if needed
-  } catch (error: any) {
-    const message = error.response?.data?.message || 'Failed to update course exam';
-    throw new Error(message);
-  }
+  return await apiRequest(courseAxios, 'put', `/${schoolName}/courses/${courseId}/exams`, {
+    examId,
+    examType,
+  });
 };

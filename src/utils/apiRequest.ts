592f84e698157eeb6c235cc3038e455bb3a4a0c2
midhunpallampetty <midhunpallampetty@gmai.com>
2025-09-23 10:17:23 +0530

    updated reset password

--- src/utils/apiRequest.ts
+++ src/utils/apiRequest.ts
@@ -1,19 +1,29 @@
 // src/api/base.ts
-import { AxiosInstance } from 'axios';
+import { AxiosInstance, AxiosRequestConfig } from 'axios';
 
-type HttpMethod = 'get' | 'post' | 'put' | 'patch' | 'delete';
+type HttpMethod =
+  | 'get'
+  | 'post'
+  | 'put'
+  | 'patch'
+  | 'delete'
+  | 'options'
+  | 'head'
+  | 'trace';
 
 export const apiRequest = async <T>(
   axiosInstance: AxiosInstance,
   method: HttpMethod,
   url: string,
   data?: any,
-  config?: any
+  config?: AxiosRequestConfig
 ): Promise<T> => {
   const response = await axiosInstance.request<T>({
     method,
     url,
-    ...(method === 'get' || method === 'delete' ? {} : { data }),
+    ...(method === 'get' || method === 'delete' || method === 'head' || method === 'options' || method === 'trace'
+      ? {}
+      : { data }),
     ...config,
   });
 

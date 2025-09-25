dd6c18070e7542ebe698c86200054b643f1c5a66
midhunpallampetty <midhunpallampetty@gmai.com>
2025-09-25 21:58:41 +0530

    mvp1

--- src/features/student/components/UI/CompletionCelebration.tsx
+++ src/features/student/components/UI/CompletionCelebration.tsx
@@ -45,7 +45,7 @@ const CompletionCelebration: React.FC<CompletionCelebrationProps> = ({
         const schoolName = course?.schoolName || ''; // Get from course or context
         const courseId = course?._id || '';
           const studentId = getStudentId();
-
+console.log(studentId,'student')
         if (!schoolName || !courseId || !studentId) {
           console.error('Missing parameters for certificate fetch');
           return;
